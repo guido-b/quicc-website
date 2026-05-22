ab16fb616a5731f6daec7920280cc1e4b74a5b50
guido-b <gbellomo@gmail.com>
2026-05-22 17:56:15 -0300

    fix images location

---
+++ src/data/teamImages.ts
@@ -0,0 +1,26 @@
+import bellomo from '../assets/team/bellomo.png';
+import bosyk from '../assets/team/bosyk.png';
+import cifuentes from '../assets/team/cifuentes.jpg';
+import berzzotti from '../assets/team/berzzotti.jpg';
+import bruno from '../assets/team/bruno.jpg';
+import ciancaglini from '../assets/team/ciancaglini.png';
+import figueira from '../assets/team/figueira.png';
+import losada from '../assets/team/losada.png';
+import meroi from '../assets/team/meroi.jpg';
+import santesteban from '../assets/team/santesteban.jpg';
+import bendersky from '../assets/team/bendersky.png';
+
+
+export const teamImages = {
+  'bellomo.png': bellomo,
+  'bosyk.png': bosyk,
+  'cifuentes.jpg': cifuentes,
+  'berzzotti.jpg': berzzotti,
+  'bruno.jpg': bruno,
+  'ciancaglini.png': ciancaglini,
+  'figueira.png': figueira,
+  'losada.png': losada,
+  'meroi.jpg': meroi,
+  'santesteban.jpg': santesteban,
+  'bendersky.png': bendersky,
+};
